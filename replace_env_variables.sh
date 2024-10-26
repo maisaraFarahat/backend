6b1e90e44b9cfbaff29f44708d8c0936c326bf1b
Maisara Farahat <maisara.farahat@gmail.com>
2024-10-26 03:21:59 +0300

    hotfix

--- replace_env_variables.sh
+++ replace_env_variables.sh
@@ -3,11 +3,6 @@
 # Define the path to your app.yaml file
 APP_YAML_FILE="app.yaml"
 
-# Ensure all environment variables are available
-if [[ -z "${DATABASE_URL}" || -z "${SENDGRID_API_KEY}" || -z "${SENDGRID_VERIFIED_SENDER}" || -z "${ENCRYPTION_ROUNDS}" || -z "${JWT_SECRET}" ]]; then
-  echo "One or more environment variables are missing. Please check your GitHub secrets."
-  exit 1
-fi
 
 # Replace placeholders in app.yaml with corresponding environment variables
 sed -i "s|{{DATABASE_URL}}|${DATABASE_URL}|g" $APP_YAML_FILE
