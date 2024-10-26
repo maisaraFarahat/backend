2a52881aa75d52351ce1687870f5fe183e3c7d66
Maisara Farahat <maisara.farahat@gmail.com>
2024-10-26 03:37:59 +0300

    hotfix

--- replace_env_variables.sh
+++ replace_env_variables.sh
@@ -4,16 +4,16 @@
 APP_YAML_FILE="app.yaml"
 
 # Ensure all environment variables are available
-if [[ -z "${{secrets.DATABASE_URL}}" || -z "${{secrets.SENDGRID_API_KEY}}" || -z "${{secrets.SENDGRID_VERIFIED_SENDER}}" || -z "${{secrets.ENCRYPTION_ROUNDS}}" || -z "${{secrets.JWT_SECRET}}" ]]; then
+if [[ -z "${DATABASE_URL}" || -z "${SENDGRID_API_KEY}" || -z "${SENDGRID_VERIFIED_SENDER}" || -z "${ENCRYPTION_ROUNDS}" || -z "${JWT_SECRET}" ]]; then
   echo "One or more environment variables are missing. Please check your GitHub secrets."
   exit 1
 fi
 
 # Replace placeholders in app.yaml with corresponding environment variables
-sed -i "s|{{DATABASE_URL}}|${{secrets.DATABASE_URL}}|g" $APP_YAML_FILE
-sed -i "s|{{SENDGRID_API_KEY}}|${{secrets.SENDGRID_API_KEY}}|g" $APP_YAML_FILE
-sed -i "s|{{SENDGRID_VERIFIED_SENDER}}|${{secrets.SENDGRID_VERIFIED_SENDER}}|g" $APP_YAML_FILE
-sed -i "s|{{ENCRYPTION_ROUNDS}}|${{secrets.ENCRYPTION_ROUNDS}}|g" $APP_YAML_FILE
-sed -i "s|{{JWT_SECRET}}|${{secrets.JWT_SECRET}}|g" $APP_YAML_FILE
+sed -i "s|{{DATABASE_URL}}|${DATABASE_URL}|g" $APP_YAML_FILE
+sed -i "s|{{SENDGRID_API_KEY}}|${SENDGRID_API_KEY}|g" $APP_YAML_FILE
+sed -i "s|{{SENDGRID_VERIFIED_SENDER}}|${SENDGRID_VERIFIED_SENDER}|g" $APP_YAML_FILE
+sed -i "s|{{ENCRYPTION_ROUNDS}}|${ENCRYPTION_ROUNDS}|g" $APP_YAML_FILE
+sed -i "s|{{JWT_SECRET}}|${JWT_SECRET}|g" $APP_YAML_FILE
 
 echo "Environment variables have been replaced in $APP_YAML_FILE."
